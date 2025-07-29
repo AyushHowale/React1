5b914c5e427be7900e711a90eed243d759d998b9
AyushHowale <118345207+AyushHowale@users.noreply.github.com>
2025-07-29 16:54:21 +0530

    Completed todo app using redux-toolkit

--- Redux-toolkit/src/App.jsx
+++ Redux-toolkit/src/App.jsx
@@ -2,17 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todos from './Components/Todos'
+import Addtodo from './Components/Addtodo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <>
-   <div>
+   
     <h1 className='text-3xl text-center p-4 text-red-600 bg-green-700'>
     Use redux tool kit
     </h1>
-    </div>
+    
+    <Addtodo />
+    <Todos />
+    
+    
    </>
   )
 }
